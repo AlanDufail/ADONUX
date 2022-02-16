dde6245b7c1cb0f077efeb206b5e51904afe9b91
DUFAIL Alan <Alan.DUFAIL@adp.fr>
2022-02-16 13:37:32 +0100

    adding new loader animation & fix top_section animation

---
+++ dist/index.35f88cf3.js
@@ -0,0 +1,8 @@
+setTimeout(function() {
+    document.getElementById("loading-page").className += "loaded";
+    document.getElementById("loader").className += "opzero";
+    document.getElementById("lastray").className += " finalray";
+    document.body.className += "whitebk";
+}, 6500);
+
+//# sourceMappingURL=index.35f88cf3.js.map
